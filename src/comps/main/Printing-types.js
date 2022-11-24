64fe6d112fa5e064194c14684a3b89cbbc820442
SadnanRashid <sadnan.rashid.07@gmail.com>
2022-11-25 01:08:46 +0600

    added image preview

--- src/comps/main/Printing-types.js
+++ src/comps/main/Printing-types.js
@@ -2,20 +2,69 @@ import "./main.css";
 import React, { useState } from "react";
 
 export default function PrintingTypes() {
-  const [data, setData] = useState({});
+  let key = 0;
+  const [data, setData] = useState([]);
+  const [test, setTest] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [image, setImage] = useState(null);
+  function handleSelectChange(event) {
+    setTest(event.target.value);
+    console.log(test);
+  }
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setSelectedFile(event.target.files[0]);
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+  };
+  const handleDone = () => {
+    console.log(data);
+    const length = data.length;
+    let flag = false;
+    for (let i = 0; i < length; i++) {
+      if (data[i].test === test) {
+        flag = true;
+      }
+    }
+    // input data if it is not already existing
+    if (flag === false) {
+      setData((current) => [
+        ...current,
+        {
+          test,
+          image,
+        },
+      ]);
+    } else {
+      // console.log("already ini");
+      window.alert(
+        "“Printing type already added, select some other printing type"
+      );
+    }
+  };
   return (
     <div>
+      {/* <img src={image} alt="preview image" /> */}
+      <div className="d-flex flex-row flex-wrap mt-1 justify-content-center">
+        {data.map((e) => {
+          key += 1;
+          return (
+            <div className="image-div mx-2 mb-3 mt-4" key={key}>
+              <img src={e.image} alt="preview image" />
+              <p>{e.test}</p>
+            </div>
+          );
+        })}
+      </div>
       <div className="d-flex flex-row justify-content-between mt-5">
         <h3 className="fw-bold">Printing Types: </h3>
-        {/* <p className="text-danger me-4"></p> */}
-        <button
-          type="submit"
-          className="btn btn-dark font-weight-bold mt-2"
+        <p
+          className="text-danger me-4 pointer-cursor"
           data-bs-toggle="modal"
           data-bs-target="#exampleModal"
         >
-          Submit
-        </button>
+          Add printing type
+        </p>
       </div>
       {/* modal */}
       <div>
@@ -32,7 +81,7 @@ export default function PrintingTypes() {
               <div className="modal-content">
                 <div className="modal-header">
                   <h5 className="modal-title" id="exampleModalLabel">
-                    Service Confermation
+                    Add a printing type
                   </h5>
                   <button
                     type="button"
@@ -41,14 +90,44 @@ export default function PrintingTypes() {
                     aria-label="Close"
                   />
                 </div>
-                <div className="modal-body">Modal Body</div>
+                <div className="modal-body">
+                  <div className="d-flex flex-column">
+                    <select
+                      className="form-select"
+                      value={test}
+                      onChange={handleSelectChange}
+                    >
+                      <option defaultValue>Select a printing type</option>
+                      <option value={"Engraved Roller printing"}>
+                        Engraved Roller printing
+                      </option>
+                      <option value={"Screen printing"}>Screen printing</option>
+                      <option value={"Stencil printing"}>
+                        Stencil printing
+                      </option>
+                      <option value={"Block printing"}>Block printing</option>
+                      <option value={"Spray printing"}>Spray printing</option>
+                    </select>
+                    {/* <button className="btn btn-outline-dark ms-2">
+                      Add Image
+                    </button> */}
+                    <input
+                      type="file"
+                      id="myfile"
+                      name="myfile"
+                      className="btn btn-outline-dark mt-3"
+                      onChange={onImageChange}
+                    ></input>
+                  </div>
+                </div>
+
                 <div className="modal-footer">
                   <button
                     type="button"
                     className="btn btn-secondary"
-                    data-bs-dismiss="modal"
+                    onClick={handleDone}
                   >
-                    Close
+                    Done
                   </button>
                 </div>
               </div>
